b4232379ebb7ad5f640232dd5c04705d7b67f3e7
DaheeKim <iop6316@gmail.com>
2021-05-08 00:40:35 +0900

    typeScript, eslint 설치 및 적용

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "./tsconfig.json",
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "ignorePatterns": ["dist/", "node_modules/", ".eslintrc.js"],
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
